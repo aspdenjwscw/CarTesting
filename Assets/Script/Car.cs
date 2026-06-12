2b05184491a070d8908e801e89eb7cedc17d6502
FinlayWSCW <159732365+Watsonfi1@users.noreply.github.com>
2026-06-12 13:44:02 +1200

    Car.cs updates

--- Assets/Script/Car.cs
+++ Assets/Script/Car.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class GearInfo
+{
+    public float acceleration;
+    public float topSpeed;
+
+
+}
+
 public class Car : MonoBehaviour{
     public Rigidbody rigid;
     public WheelCollider wheel1, wheel2, wheel3, wheel4;
     public float drivespeed, steerspeed;
     float horizontalInput, verticalInput, verticalForward, verticalBackwards, brakingForce, brakesActive;
     private KeyCode forwardKey, backwardsKey, brakingKey;
-    
+    public GearInfo[] gearInfo;
+    public int currentGear;
+
     void Start(){
         forwardKey = KeyCode.W; //change forward to equal a different variable that we import from the settings menu later on.
-                             //This is just temporary until we make a settings menu, but will make it easier to change to when we do.
+                                //This is just temporary until we make a settings menu, but will make it easier to change to when we do.
         backwardsKey = KeyCode.S;
         brakingKey = KeyCode.Space;
         brakingForce = 100f;
@@ -45,15 +55,17 @@ public class Car : MonoBehaviour{
             brakesActive= 0f;
         }
         //There are ways to change what the Input.GetAxis("Vertical"); need to be pressed to give change them
-        verticalInput = verticalBackwards + verticalForward;
+        verticalInput = verticalBackwards + verticalForward; 
 
         horizontalInput = Input.GetAxis("Horizontal"); //Can also do the same for Horizontal at some point with the same as vertical
                                                        //also could try to put it in a loop to reduce code length
+
+
     }
 
     void FixedUpdate()
     {
-        float motor = verticalInput * drivespeed;
+        float motor = verticalInput * drivespeed; //You could make it if moving foward, and also your pressing backwards it will break before automatically switching to reversing.
         wheel1.motorTorque = motor;
         wheel2.motorTorque = motor;
         wheel3.motorTorque = motor;
--- Assets/Script/Wheels.cs
+++ Assets/Script/Wheels.cs
@@ -13,7 +13,8 @@ public class Wheels : MonoBehaviour
             wheelMesh.localEulerAngles = new Vector3(wheelMesh.localEulerAngles.x, wheelCollider.steerAngle - wheelMesh.localEulerAngles.z + 90f, wheelMesh.localEulerAngles.z);
         }
         wheelMesh.Rotate(0, wheelCollider.rpm / 60 * 360 * Time.deltaTime, 0);
-
+        //Add something that detects the surface you're on and changes the stiffness
+        //Mostly difficult due to having to find a way to detect the surface change.
 
 
     }
