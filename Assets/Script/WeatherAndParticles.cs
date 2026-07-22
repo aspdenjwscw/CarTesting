ee25176eb4f083ca2d8a43e649d2a2f23abc6669
FinlayWSCW <159732365+Watsonfi1@users.noreply.github.com>
2026-07-22 12:11:45 +1200

    Updated skybox changing for James to pull

--- Assets/Script/WeatherAndParticles.cs
+++ Assets/Script/WeatherAndParticles.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
 
-public class WeatherAndParticles : Monobehaviour
+public class WeatherAndParticles : MonoBehaviour
 {
     private static Material mainDaySkyBox;
     private static Material rainDaySkyBox;
     private static Material mainNightSkyBox;
     private static Material mainSunsetSkyBox;
 
-    void static mainDaySkyBox = SkyBoxes.Load<Material>("mainDay")
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    private static void Initialize()
+    {
+        mainDaySkyBox = Resources.Load<Material>("mainDay");
+
+
+        //The Update class won't work without an object so this makes one that won't be deleted between scenes to make sure it all works.
+        GameObject bootstrapper = new GameObject("WeatherSystem_Runtime");
+        bootstrapper.AddComponent<WeatherAndParticles>();
+        DontDestroyOnLoad(bootstrapper);
+    }
+    
+
+
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.L))
+        //Checking if its working, and the null is so it only works if the mainDaySkybox has acutally been defined
+        if (Input.GetKey(KeyCode.L) && mainDaySkyBox != null)
         {
             RenderSettings.skybox = mainDaySkyBox;
+            //Redos all the lighting so the skybox changes will be applied properly to everything.
+            DynamicGI.UpdateEnvironment();
         }
         if (Input.GetKey(KeyCode.K))
         {
