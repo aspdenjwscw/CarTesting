65006c93019e43e05c92e076ea902703e0749118
FinlayWSCW <159732365+Watsonfi1@users.noreply.github.com>
2026-07-28 10:10:17 +1200

    Redone Weather and Particles.cs - Finlay

--- Assets/Script/WeatherAndParticles.cs
+++ Assets/Script/WeatherAndParticles.cs
@@ -7,18 +7,31 @@ public class WeatherAndParticles : MonoBehaviour
     private static Material mainNightSkyBox;
     private static Material mainSunsetSkyBox;
 
+    private static Light directionalLight;
+
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     private static void Initialize()
     {
         mainDaySkyBox = Resources.Load<Material>("mainDay");
+        rainDaySkyBox = Resources.Load<Material>("rainDay");
+        mainNightSkyBox = Resources.Load<Material>("mainNight");
+        mainSunsetSkyBox = Resources.Load<Material>("mainSunset");
+
+        GameObject lightObject = new GameObject("DownDirectionalLight");
+        directionalLight = lightObject.AddComponent<Light>();
+        directionalLight.type = LightType.Directional;
+        directionalLight.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        directionalLight.intensity = 1f;
 
 
         //The Update class won't work without an object so this makes one that won't be deleted between scenes to make sure it all works.
         GameObject bootstrapper = new GameObject("WeatherSystem_Runtime");
         bootstrapper.AddComponent<WeatherAndParticles>();
         DontDestroyOnLoad(bootstrapper);
+
+        DontDestroyOnLoad(lightObject);
     }
-    
+
 
 
 
@@ -29,6 +42,7 @@ public class WeatherAndParticles : MonoBehaviour
         {
             RenderSettings.skybox = mainDaySkyBox;
             //Redos all the lighting so the skybox changes will be applied properly to everything.
+            directionalLight.intensity = 1f;
             DynamicGI.UpdateEnvironment();
         }
         if (Input.GetKey(KeyCode.K))
